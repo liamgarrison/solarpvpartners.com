e0b1ef11b1b8e67b41b99790c2ba0a63d55ed90a
Liam Garrison <liamgarrison@gmail.com>
2023-08-08 15:08:14 -0400

    remove afghanistan

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,8 +21,8 @@ const About = () => {
           retail, IT, manufacturing, government and distribution sectors. Our
           purpose is to improve energy independence and deploy sustainable,
           renewable resources for the improvement of our environment. SPVP
-          partners with companies in the UK/Ireland, Afghanistan, the Caribbean,
-          Czech Republic, Estonia, Kenya, Malta, Mexico, Netherlands, Nigeria,
+          partners with companies in the UK/Ireland, the Caribbean, Czech
+          Republic, Estonia, Kenya, Malta, Mexico, Netherlands, Nigeria,
           Netherlands, Norway, Portugal, Sierra Leone, Zimbabwe, UAE and the USA
           to install safe, non-toxic battery technology solutions with Solar PV
           systems, for both on-grid and off-grid locations.
